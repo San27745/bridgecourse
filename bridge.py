cebea4d31d5bf9bb669fbb82a9d4033c8910a027
San27745 <kalesanika241@gmail.com>
2026-09-15 11:13:12 +0530

    PythonFile

---
+++ bridge.py
@@ -0,0 +1,43 @@
+a=int(input("Enter number"))
+print("Number is odd",a%2!=0)
+
+while True:
+    string=input("Enter a name")
+    print(string)
+a="some_pass"
+b=True
+while b:
+    passw=input("Enter password")
+    if passw == a:
+        b=False
+print("Password Match")
+i=0
+while i<=10:
+    print(i)
+    i+=1
+#table of 2
+i=1
+while i<=10:
+   print(i*2)
+   i+=1
+#table of 5
+i=1
+while i<=10:
+   print(i*5)
+   i+=1
+
+i=0
+print(i)
+while i<10:
+   i+=1
+   if i==5:
+      continue
+   print(i)
+
+n=[1,2,3,4,5,6]
+for a in n:
+   print(a)
+
+for i in range(1,11):
+    print(i)
+
